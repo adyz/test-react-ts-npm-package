77565af4984fe0617bdf688adea2086e9fa6b783
Adrian Florescu <florescuadyz@yahoo.com>
2018-07-24 10:47:51 +0300

    build(automation): simplified build steps

--- automation-scripts/automate-types-location.js
+++ automation-scripts/automate-types-location.js
@@ -1,6 +1,7 @@
 
 /*
- * Copy the coresponded dts file in the dist folder (because ts places that file somewhere strange)
+ * Copy the coresponded dts file in the dist folder 
+ * (because ts places the files in the src/components folder with their own components too)
  */
 const fs = require('fs-extra');
 
@@ -9,28 +10,38 @@ const fileWalker = require('./utils/fileWalker');
 
 const config = require('../config.js');
 
+
 fileWalker("./" + config.build.distFolder, function (err, files) {
-    if (err) {
-        throw err;
-    }
+    if (err) { throw err; }
+
     const filterFiles = files.filter(((file) => {
         return file.endsWith('.d.ts');
-    }))
-    console.log(filterFiles);
+}))
+
     filterFiles.forEach(fileName => {
-        console.log('element', fileName);
+
         const fileNameN = fileName.lastIndexOf('/');
-        var fileNameFinalResult = fileName.substring(fileNameN + 1).toLocaleLowerCase();
-        // With a callback:
-        fs.copy(fileName, './dist/' + fileNameFinalResult, { flag: 'w'}, err => {
-            if (err) return console.error(err)
+        const fileNameFinalResult = fileName.substring(fileNameN + 1);
+        const fileDestination = './'+config.build.distFolder+ '/' + fileNameFinalResult;
+
+        fs.copy(fileName, './'+config.build.distFolder+ '/' + fileNameFinalResult, { flag: 'w'}, err => {
+            if (err) return console.error(err);
+
+            // Replace double dots with ./src/components 
+            // if this file requires other components types
+            // except the index that goes only one flor up
+            const fileContents = fs.readFileSync(fileDestination, 'utf-8');
+            let fileContentsNew;
+            if(fileName.indexOf('index.d.ts') !== -1){
+                fileContentsNew = fileContents.replace(/(\.\.)/gim, '.');
+            } else {
+                fileContentsNew = fileContents.replace(/(\.\.)/gim, './src/components');
+            }
+        
+            //Write the file
+            fs.writeFileSync(fileDestination, fileContentsNew, 'utf-8');
 
             console.log(fileName, ' copied with success!')
-        }) // copies file
+        })
     });
 });
-
-
-console.log('Coping the d.ts files to the right location...');
-
-
